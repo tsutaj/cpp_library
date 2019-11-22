e479e15097ffa2327183531cf4f61b39f29121bb
Tsutajiro <y.sugie.15739d@gmail.com>
2019-11-23 02:29:42 +0900

    clean verifying test files

--- verifying_test/run_ci.sh
+++ verifying_test/run_ci.sh
@@ -94,6 +94,7 @@ run() {
 # CI
 git checkout master
 git remote set-url origin https://Tsutajiro:${GITHUB_TOKEN}@github.com/Tsutajiro/cpp_library.git
+
 git config --global user.name "Tsutajiro"
 git config --global user.email "y.sugie.15739d@gmail.com"
 git config --global push.default simple
@@ -112,40 +113,6 @@ if [ -n "$(git status -s)" ]; then
     last_commit="$(git log -1 | head -1 | awk '{print $2}')"
     git add ./test
     git commit -m "[auto-verifier] [ci skip] verify commit ${last_commit}"
-    git push --quiet origin master
+    git push --quiet origin master >/dev/null 2>&1
     echo "Pushed updated branch 'master'"
 fi
-
-# if [[ $# -eq 0 ]] ; then
-#     if [[ $CI ]] ; then
-#         # CI
-#         for f in $(list-recently-updated | grep aoj) ; do
-#             run $f
-#         done
-#     else
-#         # # local / github actions
-#         git config --local user.name "Tsutajiro"
-#         git config --local user.email "y.sugie.15739d@gmail.com"
-#         git branch -a
-
-#         for f in $(find . -name \*.test.cpp) ; do
-#             echo "----------------------------------------------------------------------"
-#             echo "## Testing '${f}' ..."
-#             echo "----------------------------------------------------------------------"
-#             run $f
-#         done
-
-#         git status -s
-#         if [ -n "$(git status -s)" ]; then
-#             last_commit="$(git log -1 | head -1 | awk '{print $2}')"
-#             git add ./test
-#             git commit -m "[auto-verifier] verify commit ${last_commit}"
-#             git push --quiet origin HEAD >/dev/null 2>&1
-#         fi
-#     fi
-# else
-#     # specified
-#     for f in "$@" ; do
-#         run "$f"
-#     done
-# fi
