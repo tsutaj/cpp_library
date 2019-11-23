35da701482a4b90bb5daad45e757c36bf854a825
Tsutajiro <y.sugie.15739d@gmail.com>
2019-11-23 20:19:03 +0900

    show url and dependencies

---
+++ verifying_test/check_dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CXX=${CXX:-g++}
+CXXFLAGS="${CXXFLAGS:--std=c++14 -O3}"
+ulimit -s 65532
+
+list_dependencies() {
+    file="$1"
+    $CXX $CXXFLAGS -I . -MD -MF /dev/stdout -MM "$file" | sed '1s/[^:].*: // ; s/\\$//' | xargs -n 1
+}
+
+list_defined() {
+    file="$1"
+    $CXX $CXXFLAGS -I . -dM -E "$file"
+}
+
+get_url() {
+    file="$1"
+    list_defined "$file" | grep '^#define PROBLEM ' | sed 's/^#define PROBLEM "\(.*\)"$/\1/'
+}
+
+get_error() {
+    file="$1"
+    list_defined "$file" | grep '^#define ERROR ' | sed 's/^#define ERROR "\(.*\)"$/\1/'
+}
+
+get_last_commit_date() {
+    file="$1"
+    list_dependencies "$file" | xargs git log -1 --date=iso --pretty=%ad
+}
+
+file="$1"
+list_dependencies $file
