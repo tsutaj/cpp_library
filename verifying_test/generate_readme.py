b78d946d830c0499a77d476a5332c3ccc0730e92
Tsutajiro <y.sugie.15739d@gmail.com>
2019-11-25 02:36:23 +0900

    modify py

--- verifying_test/generate_readme.py
+++ verifying_test/generate_readme.py
@@ -31,6 +31,15 @@ class TestFile:
         self.is_verified = is_verified
         self.problem = problem
 
+# Verify 済みかどうかのマーク
+def get_mark(cond, strict = True):
+    if cond:
+        return ':heavy_check_mark:'
+    elif strict:
+        return ':x:'
+    else:
+        return ':warning:'
+        
 # ディレクトリ一覧
 def get_directory_list(path):
     files = os.listdir(path)
@@ -52,7 +61,7 @@ top_path = './'
 lib_path = './library/'
 test_path = './verified/'
 
-def generate_lib_page(md_filename, lib_filepath, lib_dependencies):
+def generate_lib_page(md_filename, lib_filepath, lib_dependencies, is_verified):
     with open(md_filename, mode='w') as md_file:
         md_file.write(import_mathjax_text)
         md_file.write('\n\n')
@@ -60,7 +69,8 @@ def generate_lib_page(md_filename, lib_filepath, lib_dependencies):
         top_url = os.path.normpath(os.path.join(os.path.relpath(top_path, lib_path), 'index.html'))
         md_file.write('[トップページに戻る]({})\n\n'.format(top_url))
 
-        md_file.write('# {}\n'.format(os.path.basename(lib_filepath).replace('_', '\_')))
+        mark = get_mark(is_verified, False)
+        md_file.write('# {} {}\n'.format(mark, os.path.basename(lib_filepath).replace('_', '\_')))
         md_file.write('---\n\n')
 
         if os.path.basename(lib_filepath) in lib_dependencies:
@@ -88,7 +98,7 @@ def generate_test_page(md_filename, testfile):
         top_url = os.path.normpath(os.path.join(os.path.relpath(top_path, os.path.dirname(md_filename)), 'index.html'))
         md_file.write('[トップページに戻る]({})\n\n'.format(top_url))
 
-        mark = ':heavy_check_mark:' if testfile.is_verified else ':x:'
+        mark = get_mark(testfile.is_verified)
         md_file.write('# {} {}\n'.format(mark, testfile.testfile_name.replace('_', '\_')))
         md_file.write('---\n\n')
 
@@ -113,15 +123,16 @@ def generate_test_page(md_filename, testfile):
         md_file.write('[トップページに戻る]({})\n'.format(top_url))
                 
 # Markdown 形式で出力 (アンダースコアはそのまま使うとあれなので置換する)
-def convert_lib_to_md(dir_name, lib_file_list, lib_dependencies):
+def convert_lib_to_md(dir_name, lib_file_list, lib_dependencies, lib_verified):
     subprocess.run( 'mkdir -p {}'.format(lib_path), shell = True )
     
     print('##', dir_name)
     for f in lib_file_list:
         md_filename = os.path.join(lib_path, os.path.basename(f)) + '.md'
         html_filename = os.path.join(lib_path, os.path.basename(f)) + '.html'
-        generate_lib_page(md_filename, f, lib_dependencies)
-        print('* [{}]({})'.format(os.path.basename(f).replace('_', '\_'), html_filename))
+        is_verified = lib_verified[os.path.basename(f)] if os.path.basename(f) in lib_verified else False
+        generate_lib_page(md_filename, f, lib_dependencies, is_verified)
+        print('* {} [{}]({})'.format(get_mark(is_verified, False), os.path.basename(f).replace('_', '\_'), html_filename))
     print()
         
 def convert_test_to_md(testfile):
@@ -132,11 +143,11 @@ def convert_test_to_md(testfile):
     md_filename = os.path.normpath(os.path.join(test_path, testfile.testfile_name)) + '.md'
     html_filename = os.path.normpath(os.path.join(test_path, testfile.testfile_name)) + '.html'
     generate_test_page(md_filename, testfile)
-    mark = ':heavy_check_mark:' if testfile.is_verified else ':x:'
+    mark = get_mark(testfile.is_verified)
     print('* {} [{}]({})'.format(mark, testfile.testfile_name.replace('_', '\_'), html_filename))
         
 def get_test_dict(dir_name, test_file_list):
-    test_files, lib_dependencies = [], {}
+    test_files, lib_dependencies, lib_verified = [], {}, {}
     for f in test_file_list:
         testfile_name = f
         
@@ -158,7 +169,10 @@ def get_test_dict(dir_name, test_file_list):
 
         for d in dependencies:
             lib_dependencies.setdefault(os.path.basename(d), []).append(f)
-    return test_files, lib_dependencies
+            lib_verified.setdefault(os.path.basename(d), is_verified)
+            lib_verified[os.path.basename(d)] |= is_verified
+            
+    return test_files, lib_dependencies, lib_verified
     
 def main():
     print(import_mathjax_text)
@@ -184,7 +198,7 @@ def main():
     for d in test_dir_list:
         test_cpp_file_list = get_matched_file_list(os.path.join(test_path, d), test_cond)
         # convert_test_to_md(d, test_cpp_file_list)
-        test_files, lib_dependencies = get_test_dict(os.path.join(test_path, d), test_cpp_file_list)
+        test_files, lib_dependencies, lib_verified = get_test_dict(os.path.join(test_path, d), test_cpp_file_list)
 
     desc_1 = '''\
 # ライブラリ一覧
@@ -199,7 +213,7 @@ def main():
     lib_dir_list = get_matched_directory_list(lib_path, lib_cond, ignore_lib_list)
     for d in lib_dir_list:
         cpp_file_list = get_matched_file_list(os.path.join(lib_path, d), lib_cond)
-        convert_lib_to_md(d, cpp_file_list, lib_dependencies)
+        convert_lib_to_md(d, cpp_file_list, lib_dependencies, lib_verified)
 
     desc_2 = '''\
 # Verify ファイル一覧
