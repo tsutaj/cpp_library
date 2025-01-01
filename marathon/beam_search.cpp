d62463ddb3841f44f67778c9a78cc6d281c9c17f
tsutaj <y.sugie.15739d@gmail.com>
2025-01-01 22:44:39 +0900

    modify beam search

--- marathon/beam_search.cpp
+++ marathon/beam_search.cpp
@@ -3,45 +3,123 @@
  * @docs docs/marathon/beam_search.md
  */
 
-template <class BeamSearchState, class LiteBeamSearchState> class BeamSearch {
-  public:
-    BeamSearch() = default;
+template <typename BeamSearchState, typename LiteBeamSearchState>
+class BeamSearchBase {
+   public:
+    BeamSearchBase() = default;
+    BeamSearchBase(int beam_width) : beam_width_(beam_width) {}
     void set_beam_width(int beam_width) { beam_width_ = beam_width; }
-    void register_state(const BeamSearchState &state) {
-        states_.emplace_back(state);
-    }
-    void register_lite_state(const LiteBeamSearchState &state) {
-        lite_states_.emplace_back(state);
-        lite_states_.back().state_idx = current_state_idx_;
-    }
-    void search(const auto &add_next_lite_states, const auto &to_next_state) {
-        lite_states_.clear();
-        for(current_state_idx_ = 0; current_state_idx_ < states_.size();
-            current_state_idx_++) {
-            add_next_lite_states(states_[current_state_idx_]);
-        }
-        const int num_select = min((int)lite_states_.size(), beam_width_);
-        nth_element(lite_states_.begin(), lite_states_.begin() + num_select,
-                    lite_states_.end());
-        vector<BeamSearchState> next_states(num_select);
-        for(int i = 0; i < num_select; i++) {
-            next_states[i] = to_next_state(lite_states_[i],
-                                            states_[lite_states_[i].state_idx]);
-        }
-        states_ = move(next_states);
-    }
+    void register_state(const BeamSearchState &state) { states_.emplace_back(state); }
+    virtual void register_lite_state(LiteBeamSearchState &state) = 0;
+    virtual void search(
+        const function<void(BeamSearchState &)> &add_next_lite_states,
+        const function<BeamSearchState(LiteBeamSearchState &, BeamSearchState)> &to_next_state) = 0;
     const BeamSearchState &get_best_state() const {
+        if(states_.empty()) {
+            throw runtime_error("No states are registered.");
+        }
         return *min_element(states_.begin(), states_.end());
     }
+    const vector<BeamSearchState> &get_states() {
+        sort(states_.begin(), states_.end());
+        return states_;
+    }
+    int num_states() const { return states_.size(); }
+    virtual int num_lite_states() const = 0;
 
-  private:
+   protected:
     int beam_width_;
-    size_t current_state_idx_;
     vector<BeamSearchState> states_;
+    size_t current_state_idx_;
+};
+
+template <class BeamSearchState, class LiteBeamSearchState>
+class BeamSearch : public BeamSearchBase<BeamSearchState, LiteBeamSearchState> {
+   public:
+    using BeamSearchBase<BeamSearchState, LiteBeamSearchState>::BeamSearchBase;
+    void register_lite_state(LiteBeamSearchState &state) override {
+        state.state_idx = this->current_state_idx_;
+        lite_states_.emplace_back(state);
+    }
+    void search(
+        const function<void(BeamSearchState &)> &add_next_lite_states,
+        const function<BeamSearchState(LiteBeamSearchState &, BeamSearchState)> &to_next_state) override {
+        lite_states_.clear();
+        for(this->current_state_idx_ = 0; this->current_state_idx_ < this->states_.size();
+            this->current_state_idx_++) {
+            add_next_lite_states(this->states_[this->current_state_idx_]);
+        }
+        const int num_select = min((int)lite_states_.size(), this->beam_width_);
+        nth_element(lite_states_.begin(), lite_states_.begin() + num_select, lite_states_.end());
+        vector<BeamSearchState> next_states(num_select);
+        for(int i = 0; i < num_select; i++) {
+            next_states[i] = to_next_state(lite_states_[i], this->states_[lite_states_[i].state_idx]);
+        }
+        this->states_ = move(next_states);
+    }
+    int num_lite_states() const override { return lite_states_.size(); }
+
+   private:
     vector<LiteBeamSearchState> lite_states_;
 };
 
+template <class BeamSearchState, class LiteBeamSearchState>
+class BeamSearchWithHash : public BeamSearchBase<BeamSearchState, LiteBeamSearchState> {
+   public:
+    using BeamSearchBase<BeamSearchState, LiteBeamSearchState>::BeamSearchBase;
+    void register_lite_state(LiteBeamSearchState &state) override {
+        state.state_idx = this->current_state_idx_;
+        lite_states_.emplace(state);
+        // remove the worst (biggest) state.
+        while((int)lite_states_.size() > this->beam_width_) {
+            lite_states_.pop();
+        }
+    }
+
+    void search(
+        const function<void(BeamSearchState &)> &add_next_lite_states,
+        const function<BeamSearchState(LiteBeamSearchState &, BeamSearchState)> &to_next_state) override {
+        // NOTE: alternative of clear() because STL doesn't have priority_queue::clear().
+        lite_states_ = priority_queue<LiteBeamSearchState>();
+        hash_values_.clear();
+        for(this->current_state_idx_ = 0; this->current_state_idx_ < this->states_.size();
+            this->current_state_idx_++) {
+            add_next_lite_states(this->states_[this->current_state_idx_]);
+        }
+        const int num_select = min((int)lite_states_.size(), this->beam_width_);
+        vector<BeamSearchState> next_states(num_select);
+        int num_next_states = 0;
+        while(lite_states_.size()) {
+            auto lite_state = lite_states_.top();
+            lite_states_.pop();
+            if(hash_values_.count(lite_state.hash_value)) {
+                continue;
+            }
+            hash_values_.insert(lite_state.hash_value);
+            next_states[num_next_states++] = to_next_state(lite_state, this->states_[lite_state.state_idx]);
+        }
+        next_states.resize(num_next_states);
+        this->states_ = move(next_states);
+    }
+    int num_lite_states() const override { return lite_states_.size(); }
+
+   private:
+    priority_queue<LiteBeamSearchState> lite_states_;
+    set<ll> hash_values_;
+};
+
+template <typename score_t>
+struct BeamSearchStateBase {
+    score_t score;
+    BeamSearchStateBase() : score(0) {}
+    virtual bool operator<(const BeamSearchStateBase &state) const = 0;
+};
+
+template <typename score_t>
 struct LiteBeamSearchStateBase {
     int state_idx;
-    LiteBeamSearchStateBase() : state_idx(-1) {}
+    score_t score;
+    ll hash_value;
+    LiteBeamSearchStateBase() : state_idx(-1), score(0), hash_value(0) {}
+    virtual bool operator<(const LiteBeamSearchStateBase &state) const = 0;
 };
